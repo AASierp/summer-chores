cbc5d7174eb93284ffe0688278382f3d182f679d
Aaron Sierp <aaronsierp@gmail.com>
2025-06-03 18:38:02 -0400

    asyncAwaitVersion Created and ReadMe Updated

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 If you do not already have node installed, proceed to https://nodejs.org/en/download and download/install.
 open callbackVersion or promieseVersion in the IDE/Text Editor of your choice.
-open a new  terminal and run "node callbackVersion.js" or "node promisesVersion.js".
+open a new  terminal and run "node callbackVersion.js" , "node promisesVersion.js" or "asyncAwaitVersion.js".
+
+Each file demonstrates different ways of dealing with asynchronous programming in javascript.
+
---
+++ asyncAwaitVersion.js
@@ -0,0 +1,84 @@
+function mowYard(name){
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+        console.log(`${name} mowed the yard.`);
+        resolve();
+        }, 2000);
+    });
+    
+}
+
+function weedEat(name){
+    return new Promise((resolve, reject) => {
+            if(Math.random() < .05){
+            reject(console.log(`${name} fell asleep after mowing the yard.`));
+        }
+    else{
+            setTimeout(() => {
+            console.log(`${name} finished using the weedeater`);
+            resolve();
+            }, 1500);
+        }
+    });
+    
+}
+
+function trimHedges(name){
+    return new Promise((resolve, reject) => {
+        if(Math.random() < .10){
+        reject(console.log(`${name} fell asleep after weed eating the yard.`));
+    }
+    else{
+        setTimeout(() => {
+            console.log(`${name} finished trimming the hedges.`);
+            resolve();
+            }, 1000);
+        }
+    });
+}
+
+function collectWood(name){
+    return new Promise((resolve, reject) => {
+        if(Math.random() < .15){
+        reject(console.log(`${name} fell asleep after trimming the hedges`));
+    }
+    else{
+        setTimeout(() => {
+            console.log(`${name} finished collecting the wood`);
+            resolve();
+            }, 2500);
+        }
+    });
+}
+
+function waterGarden(name){
+    return new Promise((resolve, reject) => {
+        if(Math.random() < .20){
+        reject(console.log(`${name} fell asleep after collecting wood`));
+    }
+    else{
+        setTimeout(() => {
+            console.log(`${name} finished watering garden`);
+            resolve();
+            }, 500);
+        }
+    });
+    
+}
+
+async function doSummerChores(name) {
+  try{
+    await mowYard(name);
+    await weedEat(name);
+    await trimHedges(name);
+    await collectWood(name);
+    await waterGarden(name);
+    console.log(`${name} has completed their chores`);
+  }
+  catch(error){
+    console.error(error);
+  }
+}
+
+
+doSummerChores("Aaron");
